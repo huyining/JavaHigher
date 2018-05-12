5f3ffcc4d7534d10636aa0b410a453fc76aa4d7d
huyining <huyining@163.com>
2018-05-12 23:21:34 +0800

    kafka学习

--- kafka/src/main/java/www/topcheer/com/kafka/lesson4/internal/User.java
+++ kafka/src/main/java/www/topcheer/com/kafka/lesson4/internal/User.java
@@ -1,36 +1,51 @@
 package www.topcheer.com.kafka.lesson4.internal;
 
 /**
- * 功能描述:
- * 修订历史:
- * 版本: 1.0.0
- * 作者: huyn
- * 邮箱: huyining18@163.com
- * 日期:2018/5/11 8:35
+ * 4 byte
+ * [4][4][10][4][20]
+ *
+ *
+ * [4][4][10][4][20]
+ *
  */
-public class User {
+public class User
+{
 
-    private  int id;
+    private int id;
 
     private String name;
 
     private String address;
 
-    public User(int id, String name, String address) {
+    public User(int id, String name, String address)
+    {
         this.id = id;
         this.name = name;
         this.address = address;
     }
 
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public String getAddress() {
+    public String getAddress()
+    {
         return address;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
--- kafka/src/main/java/www/topcheer/com/kafka/lesson4/internal/UserDeserializer.java
+++ kafka/src/main/java/www/topcheer/com/kafka/lesson4/internal/UserDeserializer.java
@@ -1,34 +1,28 @@
 package www.topcheer.com.kafka.lesson4.internal;
-
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
-
-import javax.sql.rowset.serial.SerialException;
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.util.Map;
 
-/**
- * 功能描述:
- * 修订历史:
- * 版本: 1.0.0
- * 作者: huyn
- * 邮箱: huyining18@163.com
- * 日期:2018/5/11 12:58
- */
-public class UserDeserializer implements Deserializer<User>{
+
+public class UserDeserializer implements Deserializer<User>
+{
     @Override
-    public void configure(Map<String,?> map, boolean b) {
-        //do noting
+    public void configure(Map<String, ?> configs, boolean isKey)
+    {
+        //do nothing
     }
 
     @Override
-    public User deserialize(String s, byte[] date) {
-        if (date == null)
-        return null;
-        if (date.length < 12)
-            throw  new SerializationException("The User date bytes length should not be less than 12");
-        ByteBuffer buffer = ByteBuffer.wrap(date);
+    public User deserialize(String topic, byte[] data)
+    {
+        if (data == null)
+            return null;
+
+        if (data.length < 12)
+            throw new SerializationException("The User data bytes length should not be less than 12.");
+
+        ByteBuffer buffer = ByteBuffer.wrap(data);
         int id = buffer.getInt();
         int nameLength = buffer.getInt();
         byte[] nameBytes = new byte[nameLength];
@@ -36,16 +30,15 @@ public class UserDeserializer implements Deserializer<User>{
         String name = new String(nameBytes);
 
         int addrLength = buffer.getInt();
-        byte[] addressBytes = new byte[addrLength];
-        buffer.get(addressBytes);
-        String address = new String(addressBytes);
-        return new User(id,name,address);
-
-
+        byte[] addrBytes = new byte[addrLength];
+        buffer.get(addrBytes);
+        String address = new String(addrBytes);
+        return new User(id, name, address);
     }
 
     @Override
-    public void close() {
-
+    public void close()
+    {
+        //do nothing
     }
 }
